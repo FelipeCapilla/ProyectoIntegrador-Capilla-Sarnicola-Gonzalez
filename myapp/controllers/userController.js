8747c8522ec34a81f199f5b1c94b7c14ee53fe35
sarnicola <francosarnicola@MacBookPro.fibertel.com.ar>
2025-04-06 13:05:02 -0300

    register listo

---
+++ myapp/controllers/userController.js
@@ -0,0 +1,8 @@
+let userController = {
+    'register' : function (req, res) {
+        res.render('register')
+    }
+};
+
+
+module.exports = userController;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+let userController = require('../controllers/userController.js')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/register', userController.register)
 
 module.exports = router;
