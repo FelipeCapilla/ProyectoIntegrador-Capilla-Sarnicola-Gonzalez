0415fee912dfdb48cfaf1802c8a4e952a9740dc7
sarnicola <francosarnicola@MacBookPro.fibertel.com.ar>
2025-04-06 13:47:56 -0300

    listo product-add

---
+++ myapp/controllers/productAddController.js
@@ -0,0 +1,7 @@
+let productAddController = {
+    'product-add' : function(req, res){
+        res.render('product-add')
+    }
+}
+
+module.exports = productAddController;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 let userController = require('../controllers/userController.js')
-let loginController = require('../controllers/loginController.js')
+let loginController = require('../controllers/loginController.js');
+let productAddController = require('../controllers/productAddController.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,5 +11,6 @@ router.get('/', function(req, res, next) {
 
 router.get('/register', userController.register)
 router.get('/login',loginController.login)
+router.get('/product-add',productAddController['product-add'])
 
 module.exports = router;
