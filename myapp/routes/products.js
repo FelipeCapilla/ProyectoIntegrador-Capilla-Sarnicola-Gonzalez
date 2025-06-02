028e0cab8b4def072bf9be670784c778e13508f4
felixgp1 <fgonzalezprandi@udesa.edu.ar>
2025-06-02 11:53:11 -0300

    productsjs

--- myapp/routes/products.js
+++ myapp/routes/products.js
@@ -11,3 +11,36 @@ router.get('/product:id', productController.detail)
 
 
 module.exports = router;
+
+module.exports = (sequelize, DataTypes) => {
+    const Product = sequelize.define(
+      "Product",
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        usuario_id: {
+          type: DataTypes.INTEGER
+        },
+        nombre: {
+          type: DataTypes.STRING(500)
+        },
+        descripcion: {
+          type: DataTypes.STRING(1000)
+        },
+        imagen: {
+          type: DataTypes.STRING(500)
+        }
+      },
+      {
+        tableName: "productos",
+        timestamps: false
+      }
+    );
+  
+   
+  
+    return Product;
+  };
