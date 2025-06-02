88e184fb61390631b71c9db7334bf5eea373d338
sarnicola <fsarnicola@udesa.edu.ar>
2025-06-02 11:24:39 -0300

    wip

--- myapp/controllers/indexController.js
+++ myapp/controllers/indexController.js
@@ -2,6 +2,15 @@ const db = require("../database/models")
 
 let loginController = {
     index: function(req, res, next) {
+      let associate = {
+        include: [
+          { association: "texto"},
+          { association: "usuario"}
+        ],
+        order: [["createAt", "DESC"]],
+        limit: 15
+      }
+    
       db.Product.findAll()
       .then(function(productos){
         res.render('index', {productos: productos, logueado:false, usuario: req.session.user });
@@ -10,6 +19,7 @@ let loginController = {
         console.log(error);
       })
     }
+    
 }
 
 module.exports = loginController;
